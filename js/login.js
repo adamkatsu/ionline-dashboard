a48c0bba0d597e099241d790afb136d0a0f59f85
Adam Katsu <adamkatsu369@gmail.com>
2024-06-20 20:27:41 +0800

    password visibility function

--- js/login.js
+++ js/login.js
@@ -178,3 +178,12 @@ function doIECompatibility(){
         }
     }
 }
+
+function showPassword() {
+    let x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
